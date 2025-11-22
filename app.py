690e2d24d5f891037af6ee557876623c07432f76
Prabony AI <founder@prabonyai.in>
2025-11-23 01:54:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ google = oauth.register(
 )
 
 # --- configuration ---
-genai.configure(api_key=os.getenv.GEMINI_API_KEY)
+genai.configure(api_key=os.getenv("GEMINI_API_KEY")
 model = genai.GenerativeModel("models/gemini-2.0-flash")
 
 WORKSPACE_DIR = os.path.dirname(os.path.abspath(__file__))
